013c02bc8c3dbeca2f8077ea54f6e65cb3167ed9
Ignacio Rivas <rivasign@gmail.com>
2012-07-09 14:45:43 -0100

    Removed test routes

--- app.js
+++ app.js
@@ -28,8 +28,6 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
-app.get('/test', routes.test);
-
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
