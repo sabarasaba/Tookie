38d4c97eb3a035e022e63bc553ef7f78e39f282c
Ignacio Rivas <rivasign@gmail.com>
2012-07-09 20:36:07 -0100

    Fixed a bug when the resultset from the api call to tmdb is empty.

--- routes/index.js
+++ routes/index.js
@@ -85,20 +85,21 @@ var fetchData = function(res){
 					tmdb.Movie.search({query: movie.title}, function(err,result) {
 						var poster = "";
 
-						try{
-						if (result[0].posters.length >= 3)
-							poster = result[0].posters[3].image.url;
-						else
-							poster = result[0].posters[result[0].posters.length - 1].image.url;
+						try
+						{
+							if (result[0].posters.length >= 3)
+								poster = result[0].posters[3].image.url;
+							else
+								poster = result[0].posters[result[0].posters.length - 1].image.url;
+
+							var element = {url: movie.url, title: result[0].name, description: result[0].overview, poster: poster, rating: result[0].rating};
+
+							array2.push(element);
 						}
 						catch(err)
 						{
 							poster = "";
 						}
-
-					  	var element = {url: movie.url, title: result[0].name, description: result[0].overview, poster: poster, rating: result[0].rating};
-
-						array2.push(element);
 
 						callback();
 					});
